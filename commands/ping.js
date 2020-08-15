15864e2a109cb3238c7dfa0790afdbf2827acc9c
egguzchan <68133047+egguzchan@users.noreply.github.com>
2020-08-15 01:38:17 -0400

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -4,8 +4,7 @@ module.exports = {
     cooldown: 5,
 	execute(message, args) {
 		const apiPing = Math.round(message.client.ping); // This will round the api ping of the client
-		const responseTime = Math.round(Date.now() - message.createdTimestamp); // This will round the
-		// response time between when the message was received and when the message was sent
+		const responseTime = Math.round(Date.now() - message.createdTimestamp); // This will round the// response time between when the message was received and when the message was sent
 
 // You can display as
 		message.channel.send(`**API Ping:** \`${apiPing}\`\n**Response Time:** \`${responseTime}ms\``)
