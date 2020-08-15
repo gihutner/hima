531e23a44b3dd1bbcfd724d2063b77597bc15a46
egguzchan <68133047+egguzchan@users.noreply.github.com>
2020-08-15 08:04:28 -0400

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,17 +1,23 @@
 const Discord = require('discord.js');
+const {client} = require("../index");
 
 module.exports = {
 	name: 'ping',
 	description: 'Ping!',
 	cooldown: 0,
 	execute: function (message, args) {
+		const ping = Date.now() - message.createdTimestamp + " ms";
 		const ping_embed = new Discord.MessageEmbed()
 			.setColor(`#FFFEC8`)
 			.setTitle('Pong!')
+			.addField(
+				{ name: '**Uptime:**', value: client.uptime}
+			)
 			.setDescription(`beep boop`)
 
 		message.reply(ping_embed);
 	},
 };
+
 
 
