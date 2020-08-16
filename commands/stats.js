50912bc7dd3b50165070c67f1ba8fd4fbcba8cda
egguzchan <68133047+egguzchan@users.noreply.github.com>
2020-08-15 23:33:03 -0400

    Update stats.js

--- commands/stats.js
+++ commands/stats.js
@@ -19,7 +19,7 @@ module.exports = {
         const stats_embed = new Discord.MessageEmbed()
             .setColor('#FFFEC8')
             .setTitle('<:hima_chickheart:735262386893750302>・Stats!・')
-            .setDescription(`. . . . . . . . . . . . . . . . . . . . . . . . \n **Uptime** ・ ${uptime} \n **Version** ・ 1.0.0 \n **Created on** ・ ${client.user.createdAt}`)
+            .setDescription(`. . . . . . . . . . . . . . . . . . . . . . . . \n **Uptime** ・ ${uptime} \n **Version** ・ 1.0.0 \n **Created on** ・ Tue Apr 07 2020`)
             .setFooter('© eggu#0001', 'https://cdn.discordapp.com/avatars/697195772919414884/734abe1afbb98ad3f140fcff8da84416.png')
 
         message.reply(stats_embed);
