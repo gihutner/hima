c6eafbe3a08efa3e271269a7a68e7e3b7ecef19e
egguzchan <68133047+egguzchan@users.noreply.github.com>
2020-08-17 15:12:39 -0400

    17082020 update

--- commands/eval.js
+++ commands/eval.js
@@ -19,22 +19,33 @@ const no_perms = new Discord.MessageEmbed()
             message.reply(no_perms);
         } else {
 
-        let evaled; // create an evaled variable
-        try { // try to do the following:
-            evaled = await eval(args.join(' '));
-            // wait for the eval function to execute the code. This is done within the base of javascript. As parameter, we pass through all arguments and paste them together with a space.
-            // So "test 1 2 3" will be pasted in like it should, as args is an array(["test", "1", "2", "3"]), it'll become "test 1 2 3", get it? Awesome
-            message.channel.send(inspect(evaled));
-            // send the output of the evaluation in the channel through the inspect function built into the utils.
-            // More about the function here cause it's lengthy: https://nodejs.org/api/util.html#util_util_inspect_object_options
+            try {
 
+
+            const toEval = args.join(" ")
+            const evaluated = eval(toEval)
+
+            let embed = new Discord.MessageEmbed()
+                .setColor('#FFFEC8')
+                .setTimestamp()
+                .setTitle("Eval")
+                .addField("To evaluate:", `\`\`\`js\n${beautify(args.join(" "), { format: "js"})}\n\`\`\``)
+                .addField("Evaluated:", evaluated)
+                .addField("Type of:", typeof(evaluated));
+
+            await message.channel.send(embed);
             console.log(inspect(evaled));
             // also send the same output to console. You don't have to do this but you could keep it this way if you keep console open cause eval can get lengthy and sometimes not
             // go through as channel message(message may have max. 2000 characters, console has no limit)
 
         } catch (error) { // if trying failed, do the following instead (if you catch an error):
             console.error(error); // log the error to console
-            message.reply(`there was an error during evaluation:\n\`${error.message}\``); // send a message in the channel stating there was an error
+                let embed = new MessageEmbed()
+                    .setColor("#FFFEC8")
+                    .setTitle("\:x: Error!")
+                    .setDescription(e)
+
+                await message.channel.send(embed); // send a message in the channel stating there was an error
         }
     }
 }
