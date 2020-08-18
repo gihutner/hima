0d4db7379ab0bb673d1ac252f1592f288551a6dd
egguzchan <68133047+egguzchan@users.noreply.github.com>
2020-08-18 07:45:34 -0400

    Delete welcome.js

--- commands/welcome.js
+++
@@ -1,6 +0,0 @@
-const Discord = require('discord.js');
-const { client } = require('../index.js');
-
-client.on("guildMemberAdd", (message, member) => {
-    client.channels.cache.get('709112115176276074').send('welcome!')
-})
