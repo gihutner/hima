b8882e816b60932bb75211e803cfd57b236c46d8
egguzchan <68133047+egguzchan@users.noreply.github.com>
2020-08-20 17:17:20 -0400

    20082020

--- commands/autorespond.js
+++ commands/autorespond.js
@@ -1,3 +1,4 @@
+/*
 const Discord = require('discord.js');
 const { client } = require('../index.js');
 
@@ -5,25 +6,6 @@ module.exports = {
     name: 'addautoresponder',
     execute(message, args) {
 
-        const { autoresponders } = client;
-
-        const splitArgs = args.join(" ").split("|")
-        const trigger = splitArgs[0].trim()
-        const response = splitArgs.slice(1).join(" ").trim()
-
-        const existsAlready = autoresponders.get(trigger)
-
-        if(existsAlready){
-
-            return message.reply(`if i reply, means iara is dum`)
-
-        }
-
-        autoresponders.set(trigger.toLowerCase(), {
-            trigger,
-            response
-        })
-
-        return message.reply(`iara stinky`)
     }
 }
+*/
