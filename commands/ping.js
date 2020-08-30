06ee7632b1da4639757d74ed5f0f16c33acd9489
egguzchan <68133047+egguzchan@users.noreply.github.com>
2020-08-30 13:01:57 -0400

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -7,7 +7,7 @@ module.exports = {
 
 		const { client } = require('../index.js')
 		const response_latency = Date.now() - message.createdTimestamp + "ms"
-		const api_latency = Math.round(client.ws.ping) + "ms"
+		// const api_latency = Math.round(client.ws.ping) + "ms"
 		const ping_embed = new Discord.MessageEmbed()
 			.setColor(`#FFFEC8`)
 			.setTitle('Pong!')
