a5aa2da7bb9069f22a419c046191f4937ee10636
egguzchan <68133047+egguzchan@users.noreply.github.com>
2020-08-30 14:38:58 -0400

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,11 +1,9 @@
 const Discord = require('discord.js');
-const { client } = require('../index.js')
 
 module.exports = {
 	name: 'ping',
 	description: 'Ping!',
-	execute(message, args) {
-		const { client } = this;
+	execute(client, message, args) {
 		const response_latency = Date.now() - message.createdTimestamp + "ms"
 		// const api_latency = Math.round(client.ws.ping) + "ms"
 		const ping_embed = new Discord.MessageEmbed()
