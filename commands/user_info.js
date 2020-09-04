f36992a34799a2a68bdf9bfcac768a0b2ef034b8
egguzchan <68133047+egguzchan@users.noreply.github.com>
2020-09-03 22:29:38 -0400

    Update user_info.js

--- commands/user_info.js
+++ commands/user_info.js
@@ -5,14 +5,15 @@ module.exports = {
 	description: 'info about user',
 	aliases: ['shei'],
 	execute(Client, message, args) {
-		const user = message.author || message.mentions.users.first() || message.guild.members.get(args[0]);
+		let user = message.author || message.mentions.users.first() || message.guild.members.get(args[0]);
+		let member = message.guild.member(user);
 		const user_avatar = user.displayAvatarURL({ dynamic: true, size: 256});
 		const user_id = user.id;
 		const tag = user.tag;
 		const createdate = user.createdAt;
 		const createdate_time = user.createdTimestamp;
 		const status = user.presence.status;
-		const joinedat = user.joinedAt;
+		const joinedat = member.joinedAt;
 
 		const user_info = new Discord.MessageEmbed()
 			.setColor('#fcd9ce')
