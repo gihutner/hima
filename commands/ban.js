d6324cca12e2df7cb7e7390865561132dce6fc07
egguzchan <68133047+egguzchan@users.noreply.github.com>
2020-09-09 22:29:17 -0400

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -43,7 +43,7 @@ module.exports = {
 			Client.channels.cache.get('747096663390421106').send(ban_log);
 			const banned = new Discord.MessageEmbed()
 			.setDescription(`${member.user} was successfully banned.`)
-			message.channels.reply(banned);
+			message.reply(banned);
         } else if (!message.guild.members.cache.get(message.author.id).hasPermission("BAN_MEMBERS")) {
             const no_perms = new Discord.MessageEmbed()
 .setColor('#FFFBC0')
