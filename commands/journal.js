b1b4f21f8afc999b9638ca98de712515df8a8dd4
egguzchan <68133047+egguzchan@users.noreply.github.com>
2020-09-10 13:17:49 -0400

    Update journal.js

--- commands/journal.js
+++ commands/journal.js
@@ -7,10 +7,6 @@ module.exports = {
         if((args.length > 0) && message.author.id === '660869780634468393') {
 
             const journal_entry = args.slice(1).join(' ');
-            const the_users_name = message.mentions.users.first().username
-            const the_users_id = message.mentions.users.first().id
-            const the_users_discrim = message.mentions.users.first().discriminator
-            const the_users_avatar = message.mentions.users.first().displayAvatarURL({ dynamic: true })
 
             const d = new Date( timestamp );
             j_date = d.getHours() + ":" + d.getMinutes() + ", " + d.toDateString();
@@ -20,6 +16,7 @@ module.exports = {
             .setDescription(journal_entry)
             .setColor('#36393f')
 
+            message.delete()
             client.channels.cache.get('753663451901198386').send(journals)
         } else {
             return 'hello'
