ddd7189c21c48a53ce51e1dd7bf93be925bdbbfc
egguzchan <68133047+egguzchan@users.noreply.github.com>
2020-09-09 22:33:25 -0400

    Update unban.js

--- commands/unban.js
+++ commands/unban.js
@@ -1,47 +1,23 @@
-/*
-const Discord = require('discord.js');
-const { Client, GuildMember } = require("discord.js");
+module.exports.run = async (bot, message, args, con) => {
+	if(!bot.hasPermission(message, "BAN_MEMBERS")) return message.channel.send("You do not have permission to do this.");
+	
+	let search = args.join(" ");
+	if(!search) return message.channel.send("Please provide a valid ID or name.");
 
-module.exports = {
-	name: 'unban',
-    description: 'unban someone beech',
-    guildOnly: true,
-	execute(message, args, Client) {
-        if (message.guild.members.cache.get(message.author.id).hasPermission('ADMINISTRATOR')) {
-            const { client } = require("../index.js");
+	try {
+		let bans = await message.guild.fetchBans();
+		let banned = bans.get(search) || bans.find(u => u.tag.toLowerCase().includes(search.toLowerCase()));
+		
+		if(!banned) return message.channel.send("I could not find a banned user by this ID or name.");
 
-            let member = message.mentions.members.first().id || message.members.cache.get(args[0]) || message.mentions.members.first().id ||
-             client.members.resolve(args[0]);
-            let reason = args.slice(1).join(" ");
-            let ban =  message.guild.fetchBans();
-            let member_s = client.users.fetch(unbanned);
+		await message.guild.unban(banned);
 
-            message.guild.members.unban(id)
+		message.channel.send(`${banned.tag} has been unbanned.`);
+	} catch(e) {
+		message.channel.send(`Unban failed: ${reason}`)
+	}
+}
 
-
-
-
-        let unban_log = new Discord.MessageEmbed()
-        .setColor('#FF9F9F')
-        .setAuthor('𝐡𝐢𝐦𝐚𝐰𝐚𝐫𝐢 — ･ﾟ', `https://cdn.discordapp.com/attachments/726708672272531519/742047829006221373/hmawari.jpg`)
-        .setTitle('Member Unbanned!')
-        .setDescription(`${member.user} has been unabnned by ${message.author}.`)
-        .addFields(
-            { name: '**Reason**', value: `${reason}`}
-        )
-        .setTimestamp()
-
-        client.channels.cache.get('726966603077648464').send(unban_log);
-        }
-
-        if (!message.guild.members.cache.get(message.author.id).hasPermission("ADMINISTRATOR")) {
-            const no_perms = new Discord.MessageEmbed()
-.setColor('#FFFBC0')
-.setAuthor(`${message.author.username}#${message.author.discriminator}`, `${message.author.displayAvatarURL()}`)
-.setTitle('**Oh no!** <a:notlikethis:740955629635502141>')
-.setDescription('It seems like you don\'t have the correct permissions to use this command!')
-            message.reply(no_perms);
-        }
-    },
-};
-*/
+module.exports.help = {
+	name: "unban"
+}
