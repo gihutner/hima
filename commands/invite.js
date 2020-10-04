95871932091e3114e6cd753b82112f5bb2e7b02a
egguzchan <68133047+egguzchan@users.noreply.github.com>
2020-10-04 00:28:53 -0700

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -3,15 +3,18 @@ const Discord = require("discord.js");
 module.exports = {
   name: "invite",
   aliases: ["inv"],
+  execute(message, args) {
+    execute(message, args);
+    if (
+      message.guild.members.cache.get(message.author.id) ===
+      "660869780634468393"
+    ) {
+      const invuwu = new Discord.MessageEmbed()
+        .setColor("#2f3137")
+        .setDescription(
+          "you wanted to invite me??? me???? [click here uwu](https://discord.com/api/oauth2/authorize?client_id=747230852047437825&permissions=0&scope=bot)"
+        );
+      message.channel.send(invuwu);
+    }
+  },
 };
-execute(message, args);
-if (
-  message.guild.members.cache.get(message.author.id) === "660869780634468393"
-) {
-  const invuwu = new Discord.MessageEmbed()
-    .setColor("#2f3137")
-    .setDescription(
-      "you wanted to invite me??? me???? [click here uwu](https://discord.com/api/oauth2/authorize?client_id=747230852047437825&permissions=0&scope=bot)"
-    );
-  message.channel.send(invuwu);
-}
