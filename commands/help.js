03ef68348400441fc752978dca46e7e0bccd9542
egguzchan <68133047+egguzchan@users.noreply.github.com>
2020-10-04 00:44:27 -0700

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,36 +1,28 @@
-const Discord = require('discord.js');
-const { prefix } = require('../config.json');
-const normal_helpemb = new Discord.MessageEmbed()
-.setColor('#fcd9ce')
-    .setDescription('`[ ]` - **Mandatory** || `< >` - **Optional**')
-.setTitle('Himawari\'s Help Center')
-.addFields(
-    { name: '**My commands**', value: '`;avatar` \n `;suggest [suggestion]`' },
-)
-.setFooter('© eggu#0001')
-
-const manager_helpemb = new Discord.MessageEmbed()
-.setColor('#fcd9ce')
-.setTitle('Himawari\'s Help Center')
-    .setDescription('`[ ]` - **Mandatory** || `< >` - **Optional**')
-.addFields(
-    { name: '**My commands**', value: '`;avatar` \n `;suggest [suggestion]`' },
-    { name: '**Moderator Commands**', value: '`;kick [user] <reason>` \n `;ban [user] <reason>` \n `;delete [# of messages]` \n `;vent [content]` ', inline: true},
-    { name: '\u200B', value: '\u200B' },
-)
-.setFooter('©️ eggu#0001')
-
+const Discord = require("discord.js");
+const { prefix } = require("../config.json");
 
 module.exports = {
-    name: 'help',
-    description: 'help me beech, list my commands',
-    aliases: ['commands'],
-    usage: ['command name'],
+    name: "help",
+    description: "help me beech, list my commands",
+    aliases: ["commands"],
+    usage: ["command name"],
     execute(Client, message, args) {
-        if (message.guild.members.cache.get(message.author.id).hasPermission('MANAGE_CHANNELS')) {
-            return message.reply(manager_helpemb);
-        } else {
-            return message.reply(normal_helpemb);
-        }
-    }
+        const manager_helpemb = new Discord.MessageEmbed()
+            .setColor("#2f3137")
+            .setTitle("Help Center")
+            .setDescription("`[ ]` - **Mandatory** . `< >` - **Optional**")
+            .addFields(
+                {
+                    name: "**General Commands**",
+                    value: "`;avatar` \n `;suggest [suggestion]`",
+                    inline: true,
+                },
+                {
+                    name: "**Moderation**",
+                    value:
+                        "`;kick [user] <reason>` \n `;ban [user] <reason>` \n `;delete [# of messages]` \n `;vent [content]` ",
+                }
+            )
+            .setFooter("©️ eggu#0001");
+    },
 };
