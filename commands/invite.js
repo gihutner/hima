b7571703bfb3ebb60c0be8a4c1e8df18eca11e67
egguzchan <68133047+egguzchan@users.noreply.github.com>
2020-10-04 00:32:31 -0700

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -4,12 +4,11 @@ module.exports = {
   name: "invite",
   aliases: ["inv"],
   execute(message, args) {
-      const invuwu = new Discord.MessageEmbed()
-        .setColor("#2f3137")
-        .setDescription(
-          "you wanted to invite me??? me???? [click here uwu](https://discord.com/api/oauth2/authorize?client_id=747230852047437825&permissions=0&scope=bot)"
-        );
-      message.channel.send(invuwu);
-    }
+    const invuwu = new Discord.MessageEmbed()
+      .setColor("#2f3137")
+      .setDescription(
+        "you wanted to invite me??? me???? [click here uwu](https://discord.com/api/oauth2/authorize?client_id=747230852047437825&permissions=0&scope=bot)"
+      );
+    message.channel.send(invuwu);
   },
 };
