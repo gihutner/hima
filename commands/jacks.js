064c34c3f1b2a26b2cfdd3b3e10542f5a278fdbc
egguzchan <68133047+egguzchan@users.noreply.github.com>
2020-10-06 10:26:52 -0700

    Update jacks.js

--- commands/jacks.js
+++ commands/jacks.js
@@ -4,5 +4,6 @@ module.exports = {
     name: "sing",
     execute(client, message, args) {
         const sing = new Discord.MessageEmbed().setDescription("iara stinks");
+        message.channel.send(sing);
     },
 };
