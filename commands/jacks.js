f8d48ae623119e121cf055e351b9caacab5e4d08
egguzchan <68133047+egguzchan@users.noreply.github.com>
2020-10-06 10:24:32 -0700

    Create jacks.js

---
+++ commands/jacks.js
@@ -0,0 +1,8 @@
+const Discord = require("discord.js");
+
+module.exports = {
+    name: "sing",
+    execute(client, message, args) {
+        const sing = new Discord.MessageEmbed().setDescription("iara stinks");
+    },
+};
