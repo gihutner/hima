8cd8dd5d20c7c56c0166c5d1fac15d307cf314a2
egguzchan <68133047+egguzchan@users.noreply.github.com>
2020-10-17 10:44:14 -0700

    aaaaa

---
+++ commands/riddle.js
@@ -0,0 +1,25 @@
+const Discord = require("discord.js");
+
+module.exports = {
+    name: "riddle",
+    description: "just for riddle day",
+    execute(client, message, args) {
+        if (args.length > 0) {
+            const user = message.author;
+            const riddle = args.slice(0).join(" ");
+
+            const riddle_emb = new Discord.MessageEmbed()
+                .setColor("#2f3137")
+                .setDescription(riddle);
+
+            client.channels.cache
+                .get("747096663390421107")
+                .send("riddle answer from ${user},", riddle_emb);
+        } else {
+            const wrong_usage = new Discord.MessageEmbed().setDescription(
+                "`that was the wrong usage. try ;riddle [answer]`"
+            );
+            message.channel.send(wrong_usage);
+        }
+    },
+};
